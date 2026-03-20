a31848eae65b3da45b3a0f9c8bcf30f7c912427b
Hungkim23 <Hkim2k4@gmail.com>
2026-03-20 15:22:41 +0700

    thanh vien 4

--- app/src/main/java/com/example/btl_g9_trenlop/MainActivity.java
+++ app/src/main/java/com/example/btl_g9_trenlop/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.btl_g9_trenlop;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // AndroidManifest hiện đang trỏ tới MainActivity (package gốc).
+        // TV4 lại nằm trong com.example.btl_g9_trenlop.view.MainActivity, nên mình chuyển hướng sang đó.
+        startActivity(new Intent(this, com.example.btl_g9_trenlop.view.MainActivity.class));
+        finish();
     }
 }
